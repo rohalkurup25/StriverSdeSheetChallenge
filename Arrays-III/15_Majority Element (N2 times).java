44859807f2d6b4ef5e2f66f04edf1d2ba92097fa
rohalkurup25 <100403958+rohalkurup25@users.noreply.github.com>
2022-06-10 07:19:12 +0530

    added Moore's Algorithm

--- Arrays-III/15_Majority Element (N2 times).java
+++ Arrays-III/15_Majority Element (N2 times).java
@@ -13,3 +13,38 @@ public class Solution {
         return -1;
 	}
 }
+
+// Moore's Algorithm 
+ 
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int findMajority(int[] arr, int n) {
+        int val = arr[0];
+        int count =1;
+        for(int i=1;i<n;i++)
+        {
+            if(val==arr[i])
+                count++;
+            else
+            {
+                if(count==1)
+                {
+                    val = arr[i];
+                }
+                else
+                    count--;
+            }
+        }
+        int canCount = 0;
+        for(int x:arr)
+        {
+            if(x==val)
+                canCount++;
+        }
+        if(canCount>n/2)
+            return val;
+        else
+            return -1;
+	}
+}
