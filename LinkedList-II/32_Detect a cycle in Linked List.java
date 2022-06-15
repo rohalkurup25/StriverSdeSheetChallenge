cac7566bd537662eb9be1496addcecd1ed56377d
rohalkurup25 <100403958+rohalkurup25@users.noreply.github.com>
2022-06-15 12:38:31 +0530

    Add files via upload

---
+++ LinkedList-II/32_Detect a cycle in Linked List.java
@@ -0,0 +1,34 @@
+import java.util.* ;
+import java.io.*; 
+/*  
+
+    Following is the representation of the Singly Linked List node
+
+    class Node<T> {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+*/
+
+public class Solution {
+    
+    public static boolean detectCycle(Node<Integer> head) {
+        
+        Node slow = head; 
+        Node fast = head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+    }
+}
