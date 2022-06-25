a901827093016d97a8148547c4ed63a6a7feaa3b
rohalkurup25 <100403958+rohalkurup25@users.noreply.github.com>
2022-06-25 21:53:41 +0530

    Update 63_Find the element that appears once in a sorted array, and the rest element appears twice (Binary search).java

--- Binary Search/63_Find the element that appears once in a sorted array, and the rest element appears twice (Binary search).java
+++ Binary Search/63_Find the element that appears once in a sorted array, and the rest element appears twice (Binary search).java
@@ -9,4 +9,31 @@ public class Solution
             luckyNumber^=x;
         return luckyNumber;
     }
+    
+    //Binary Search
+    
+}
+
+
+
+import java.util.ArrayList;
+
+public class Solution 
+{
+    public static int uniqueElement(ArrayList<Integer> arr) 
+    {
+        int low = 0;
+        int high = arr.size()-2;
+        while(low<=high)
+        {
+
+            int mid = (low+high)>>1;
+                if(arr.get(mid).equals(arr.get(mid^1)))
+                    low = mid+1;
+                else
+                    high = mid-1;
+ 
+        }
+        return arr.get(low);
+    }
 }
