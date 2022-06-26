1b439cc873e77514cead73355633c5c121de8de5
rohalkurup25 <100403958+rohalkurup25@users.noreply.github.com>
2022-06-26 18:40:30 +0530

    Add files via upload

---
+++ Binary Search/64_Search element in a sorted and rotated array find pivot where it is rotated.java
@@ -0,0 +1,48 @@
+public class Solution {
+    public static int search(int arr[], int key) {
+        int n = arr.length;
+        int pivot = findPivot(arr);
+   //   System.out.println("pivot "+pivot);
+        int ans=-1;
+        if(pivot==n)
+           ans =  binarySearch(arr,key,0,n-1);
+        else
+        {
+            ans = binarySearch(arr,key,0,pivot-1);
+            if(ans==-1)
+                ans = binarySearch(arr,key,pivot,n-1);
+        }
+        return ans;
+    }
+    
+    public static int findPivot(int arr[])
+    {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            if(arr[mid]>=arr[0])
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return low;
+    }
+    
+    public static int binarySearch(int arr[],int key,int low,int high)
+    {
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            if(arr[mid]==key)
+                return mid;
+            else if(arr[mid]>key)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return -1;
+    }
+    
+}
