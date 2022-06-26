3abbeb9ba7041efae69a7e4b69b1902d2c40cf9e
rohalkurup25 <100403958+rohalkurup25@users.noreply.github.com>
2022-06-26 19:50:07 +0530

    Update 64_Search element in a sorted and rotated array find pivot where it is rotated.java

--- Binary Search/64_Search element in a sorted and rotated array find pivot where it is rotated.java
+++ Binary Search/64_Search element in a sorted and rotated array find pivot where it is rotated.java
@@ -45,4 +45,36 @@ public class Solution {
         return -1;
     }
     
+    
+}
+
+
+//Better approach 
+
+public class Solution {
+    public static int search(int arr[], int key) {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high)
+        {
+            int mid = (low+high)>>1;
+            if(arr[mid]==key)
+                return mid;
+            else if(arr[mid]>=arr[low])
+            {
+                if(key>=arr[low] && key<=arr[mid])
+                    high = mid-1;
+                else
+                    low = mid+1;
+            }
+            else
+            {
+                if(key>=arr[mid] && key<=arr[high])
+                    low = mid+1;
+                else
+                    high = mid-1;
+            }
+        }
+        return -1;
+    }
 }
