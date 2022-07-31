660e5848ecb4c9375d8ce413adc89a18059a24f4
rohalkurup25 <100403958+rohalkurup25@users.noreply.github.com>
2022-07-31 20:14:05 +0530

    Add files via upload

---
+++ String Part-II/171_Count and Say.java
@@ -0,0 +1,31 @@
+public class Solution 
+{
+   public static String writeAsYouSpeak(int n) 
+    {
+       if (n == 1)     return "1";
+       if (n == 2)     return "11";
+    String str = "11"; 
+    for (int i = 3; i <= n; i++)
+    {
+        
+        str += '$';
+        int len = str.length();
+        int cnt = 1; 
+        String tmp = ""; 
+        char []arr = str.toCharArray();            
+        for (int j = 1; j < len; j++)
+        {
+            if (arr[j] != arr[j - 1])
+            {
+                tmp += cnt + 0;
+                tmp += arr[j - 1];
+                cnt = 1;
+            }
+            else cnt++;
+        }
+        str = tmp;
+    }
+
+    return str;
+    }
+}
